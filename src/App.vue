1414774e4bb7a20ea56bed46e298c79e7303772c
J3m1th <denys.petryniak@gmail.com>
2022-08-22 18:22:52 +0300

    Hotfix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative mt-[56px] container mx-auto py-12 md:py-16 px-5">
     <header
-      class="fixed top-0 left-0 right-0 p-4 text-center backdrop-blur-lg bg-cyan-300/30"
+      class="fixed top-0 left-0 right-0 p-4 text-center backdrop-blur-lg bg-cyan-300/30 z-10"
     >
       <nav>
         <router-link to="/">Home</router-link>
