2da643f04826f3584d6bff3eb53750c18c65fe53
J3m1th <denys.petryniak@gmail.com>
2022-08-27 22:16:40 +0300

    Refactoring

--- src/components/post/CharacteristicItem.vue
+++ src/components/post/CharacteristicItem.vue
@@ -10,9 +10,9 @@
       class="w-[calc(100%_-_10.75rem)] h-5 bg-cyan-100 dark:bg-cyan-200 border-2 border-cyan-400 dark:border-cyan-700 rounded-xl overflow-hidden"
     >
       <div
-        v-for="(item, index) in options.max"
+        v-for="(item, index) in max"
         :key="index"
-        :style="{ width: `${100 / options.max}%` }"
+        :style="{ width: getScaleDivisionWidth }"
         class="relative inline-block text-[0] h-5 after:content-[''] after:absolute after:top-0 after:right-0 after:w-1 after:h-full after:bg-cyan-400 dark:after:bg-cyan-700 last:after:content-none"
         :class="{ ['bg-cyan-300 dark:bg-cyan-600']: index < value }"
       ></div>
@@ -30,7 +30,7 @@ export default {
 </script>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 defineProps({
   label: {
@@ -43,9 +43,9 @@ defineProps({
   },
 });
 
-const options = ref({
-  max: 5,
-});
+const max = ref(5);
+
+const getScaleDivisionWidth = computed(() => `${100 / max.value}%`);
 
 function isNumber(value) {
   return typeof value == 'number';
