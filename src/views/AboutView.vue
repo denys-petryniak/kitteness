033e7f6176f87fb92ec2ee9397aaa43cb410f272
Denys Petryniak <denys.petryniak@gmail.com>
2023-03-03 01:04:20 +0200

    Update title on about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="m-auto max-w-2xl">
     <h1 class="mb-8 text-center text-2xl font-bold md:text-3xl">
-      This wonderful world of cats belongs to the cutest quokka in the universe
-      :)
+      This enchanting feline realm is ruled by the most adorable quokka in the galaxy :)
     </h1>
     <img
       src="@/assets/images/quoka.jpeg"
