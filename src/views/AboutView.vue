51d353b13d68b5d8865c80df26fea9e3fbf9ec7e
Denys Petryniak <denys.petryniak@gmail.com>
2023-03-03 01:11:05 +0200

    Tidy up

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="m-auto max-w-2xl">
     <h1 class="mb-8 text-center text-2xl font-bold md:text-3xl">
-      This enchanting feline realm is ruled by the most adorable quokka in the galaxy :)
+      This enchanting feline realm is ruled by the most adorable quokka in the
+      galaxy :)
     </h1>
     <img
       src="@/assets/images/quoka.jpeg"
