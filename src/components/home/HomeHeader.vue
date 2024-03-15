60d4bd0b9f582698dd142dc8539fa5c785515a44
Denys Petryniak <denys.petryniak@gmail.com>
2024-03-15 20:32:50 +0200

    Change main heading

--- src/components/home/HomeHeader.vue
+++ src/components/home/HomeHeader.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="mb-6">
     <h1 class="mb-8 text-center text-2xl font-bold leading-10 md:text-3xl">
-      Welcome to the world of
+      Because everyday is a
       <span
         class="mr-3 underline decoration-amber-500 decoration-wavy decoration-2"
-        >CATS</span
+        >Caturday</span
       ><img
         src="@/assets/images/icons/cat.png"
         alt="Cat"
